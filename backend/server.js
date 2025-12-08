930daa79a146741428bcf207746aa5adb9adc16c
Zhiqiang Li <137836434+Virtual-Zhi@users.noreply.github.com>
2025-12-07 22:34:21 -0500

    2nd originfix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const sessions = {};
 connectDB();
 
 const server = http.createServer(async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://virtual-zhi.github.io/");
+    res.setHeader("Access-Control-Allow-Origin", "https://virtual-zhi.github.io");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
